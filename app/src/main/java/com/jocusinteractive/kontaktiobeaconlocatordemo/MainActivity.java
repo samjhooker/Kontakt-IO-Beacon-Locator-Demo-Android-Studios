a7244f05468d618ce1527ca29aa26de7f0013e58
samjhooker <samjhooker@gmail.com>
2016-09-16 11:54:31 +1200

    permissions checked for version >M

--- app/src/main/java/com/jocusinteractive/kontaktiobeaconlocatordemo/MainActivity.java
+++ app/src/main/java/com/jocusinteractive/kontaktiobeaconlocatordemo/MainActivity.java
@@ -1,21 +1,57 @@
-package com.jocusinteractive.kontaktiobeaconlocatordemo;
+    package com.jocusinteractive.kontaktiobeaconlocatordemo;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.kontakt.sdk.android.common.KontaktSDK;
 
-public class MainActivity extends AppCompatActivity {
+    import android.Manifest;
+    import android.annotation.TargetApi;
+    import android.content.DialogInterface;
+    import android.content.pm.PackageManager;
+    import android.os.Build;
+    import android.os.Bundle;
+    import android.support.v7.app.AlertDialog;
+    import android.support.v7.app.AppCompatActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    import com.kontakt.sdk.android.common.KontaktSDK;
 
-        KontaktSDK.initialize("BmhPDrHhfAGhidiMkwQTVNpqNkJDlLuv"); //from web portal
+    public class MainActivity extends AppCompatActivity {
 
-        
+        private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            KontaktSDK.initialize("BmhPDrHhfAGhidiMkwQTVNpqNkJDlLuv"); //from web portal
+
+
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("This app need location access");
+                    builder.setMessage("Please grant these permissions so the beacons can be identified");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                        }
+                    });
+                    builder.show();
+                    
+                }
+
+            }
+
+
+
+
+
+
+        }
 
 
     }
-}
