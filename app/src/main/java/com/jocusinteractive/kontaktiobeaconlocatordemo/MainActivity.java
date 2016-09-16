67958f46f4e41a49a1b9dd219cf79b3cea87367e
samjhooker <samjhooker@gmail.com>
2016-09-16 14:26:36 +1200

    iBeacons now discovered and printed in TextView

--- app/src/main/java/com/jocusinteractive/kontaktiobeaconlocatordemo/MainActivity.java
+++ app/src/main/java/com/jocusinteractive/kontaktiobeaconlocatordemo/MainActivity.java
@@ -10,12 +10,27 @@
     import android.os.Bundle;
     import android.support.v7.app.AlertDialog;
     import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.widget.TextView;
 
+    import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+    import com.kontakt.sdk.android.ble.manager.ProximityManager;
+    import com.kontakt.sdk.android.ble.manager.ProximityManagerContract;
+    import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
+    import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
     import com.kontakt.sdk.android.common.KontaktSDK;
+    import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+    import com.kontakt.sdk.android.common.profile.IBeaconRegion;
+
+    import java.util.List;
 
     public class MainActivity extends AppCompatActivity {
 
         private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+        private ProximityManagerContract proximityManager;
+
+        private TextView textView;
+
 
 
         @Override
@@ -24,6 +39,8 @@
             setContentView(R.layout.activity_main);
 
             KontaktSDK.initialize("BmhPDrHhfAGhidiMkwQTVNpqNkJDlLuv"); //from web portal
+
+            textView = (TextView) findViewById(R.id.textView);
 
 
 
@@ -41,7 +58,7 @@
                         }
                     });
                     builder.show();
-                    
+
                 }
 
             }
@@ -49,8 +66,59 @@
 
 
 
+            proximityManager = new ProximityManager(this);
+            proximityManager.setIBeaconListener(createIBeaconListener());
 
 
+
+
+        }
+
+
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            startScanning();
+        }
+
+        @Override
+        protected void onStop() {
+            proximityManager.stopScanning();
+            super.onStop();
+        }
+
+        @Override
+        protected void onDestroy() {
+            proximityManager.disconnect();
+            proximityManager = null;
+            super.onDestroy();
+        }
+
+        private void startScanning() {
+            proximityManager.connect(new OnServiceReadyListener() {
+                @Override
+                public void onServiceReady() {
+                    proximityManager.startScanning();
+                }
+            });
+        }
+
+        private IBeaconListener createIBeaconListener() {
+            return new SimpleIBeaconListener() {
+
+                @Override
+                public void onIBeaconsUpdated(List<IBeaconDevice> ibeacons, IBeaconRegion region) {
+                    super.onIBeaconsUpdated(ibeacons, region);
+                }
+
+                @Override
+                public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
+                    CharSequence text = textView.getText();
+                    textView.setText(text + "\n" + "Beacon: "+ibeacon.getName()+ ", Distance: " + ibeacon.getDistance());
+                    Log.i("Sample", "IBeacon discovered: " + ibeacon.getUniqueId());
+                }
+            };
         }
 
 
